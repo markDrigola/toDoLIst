38545b34ee787aed082094a1aea0127ec2d64be0
Max <flexagon89@gmail.com>
2016-11-24 13:42:46 +0200

    second

--- public/js/common.js
+++ public/js/common.js
@@ -5,20 +5,33 @@
         $.ajax({
             url:"/todo",
             async:true
-        }).done( function(data){
+        }).done( function(data) {
             $('p').html(data);
         });
 
         $('.addedTascs').on('click', function (event) {
+            var textVals = $('.someTextTascs').val();
+            $('.someTextTascs').val('');
+
             $.ajax({
-                url:"/todo",
+                url:"/todo?text="+ textVals +"",
                 async:true
-            }).done( function(data){
+            }).done( function(data) {
                 $('p').html(data);
             });
         });
         $("form").submit( function(event){
             event.preventDefault();
-        })
+        });
+
+        // var objNe = {
+        //     first: 123,
+        //     second: 'test'
+        // }
+        //
+        // for( var key in objNe) {
+        //     console.log(key)
+        //     console.log(objNe[key])
+        // }
     });
 })();
--- server/router.js
+++ server/router.js
@@ -7,28 +7,60 @@ function router(app, express) {
 
     mongoose.connect('mongodb://localhost/toDoList');
 
+    var db = mongoose.connection;
+
+    db.on('open', function () {
+
+    });
     var Schema = mongoose.Schema;
 
-    var blogSchema = new Schema({
-        title:  String,
-        author: String,
-        body:   String
+    var todoSchema = new Schema({
+        text:  String
     });
 
-    var blog = mongoose.model('blog', blogSchema);
 
-    var blog1 = new blog({
-        title:  "first",
-        author: "second",
-        body: "third"
-    });
+    var todoSchema = mongoose.model('text', todoSchema);
 
     app.get('/', function (req, res) {
         res.render('template');
     });
 
     app.get('/todo', function (req, res) {
-        res.render('partials/todo',{data: blog1});
+        var requestData = req.query;
+
+        var task = new todoSchema({
+            text:  requestData.text
+        });
+
+            // todoSchema.find("text", function (err,task) {
+            //     todoSchema.remove("text", function () {
+            //         console.log(task)
+            //     });
+            //     res.render('partials/todo',{data: task});
+            // });
+
+            task.save(function (err, task) {
+                if(err) {
+                    return console.error(err);
+                }
+                var tmpTasks = [];
+                todoSchema.find("text", function (err,task) {
+                    for(var i = 0; i < task.length; i++) {
+                        if(task[i].text !== "" && task[i].text !== undefined) {
+                            tmpTasks.push(task[i].text);
+                        }
+                    }
+                    res.render('partials/todo',{ data: tmpTasks });
+                });
+
+            });
+
+
+
+            // todoSchema.find("text", function (err,task) {
+            //     task.remove();
+            //     res.render('partials/todo',{data: task});
+            // });
     });
 }
 
